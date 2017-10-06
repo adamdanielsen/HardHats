59dbb764885bf3a7db04c0ccdfe8277e659466e5
adamdanielsen <adamdanielsen@gmail.com>
2017-10-05 20:46:51 -0400

    Directory changes and landscape menu (Doesn't crasha on rotation)

--- app/src/main/java/project/senior/hardhats/MenuActivity.java
+++ app/src/main/java/project/senior/hardhats/MenuActivity.java
@@ -20,8 +20,6 @@ public class MenuActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-
-
         viewPager = (ViewPager) findViewById(R.id.viewpager);
         setupViewPager(viewPager);
 
