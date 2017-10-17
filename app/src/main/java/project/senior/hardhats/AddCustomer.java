7ff29b072bdf2991f1f3e27b4bc7b16d51834dcc
Evan-E <dudeman43094@gmail.com>
2017-10-17 10:39:17 -0400

    added add customer activity

---
+++ app/src/main/java/project/senior/hardhats/AddCustomer.java
@@ -0,0 +1,13 @@
+package project.senior.hardhats;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddCustomer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_customer);
+    }
+}
