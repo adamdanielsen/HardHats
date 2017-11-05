adce73e1a64550437bf7d2338fe6175fe7ce54c8
Evan-E <dudeman43094@gmail.com>
2017-11-05 12:14:13 -0500

    Spinner element added to add customer and code for the add customer class done. need to validate the input and add the php routine to insert a new customer into the database

--- app/src/main/java/project/senior/hardhats/AddCustomer.java
+++ app/src/main/java/project/senior/hardhats/AddCustomer.java
@@ -3,8 +3,12 @@ package project.senior.hardhats;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class AddCustomer extends AppCompatActivity {
 
@@ -15,7 +19,8 @@ public class AddCustomer extends AppCompatActivity {
     EditText houseNumber;
     EditText streetName;
     EditText zipCode;
-    EditText state;
+    Spinner state;
+    String customer_State;
 
     Button save;
 
@@ -31,7 +36,11 @@ public class AddCustomer extends AppCompatActivity {
         houseNumber = (EditText) findViewById(R.id.customer_house_number_editText);
         streetName = (EditText) findViewById(R.id.customer_street_address_editText);
         zipCode = (EditText) findViewById(R.id.customer_zip_code_editText);
-        state = (EditText) findViewById(R.id.customer_state_editText);
+        state = (Spinner) findViewById(R.id.customer_state_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.states, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        state.setAdapter(adapter);
+        state.setOnItemSelectedListener(state_listener);
 
         save = (Button) findViewById(R.id.save_customer_button);
 
@@ -45,16 +54,29 @@ public class AddCustomer extends AppCompatActivity {
                 String customer_houseNumber = houseNumber.getText().toString();
                 String customer_street = streetName.getText().toString();
                 String customer_Zip = zipCode.getText().toString();
-                String customer_State = state.getText().toString();
+                if(customer_State.length() == 0) {
+                    Toast.makeText(AddCustomer.this, "No State Selected", Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(AddCustomer.this, "State Selected: " + customer_State, Toast.LENGTH_LONG).show();
+                }
+                //String customer_State = state.getText().toString();
 
 
             }
         });
-
-
-
-
-
-
     }
+
+    private AdapterView.OnItemSelectedListener state_listener = new AdapterView.OnItemSelectedListener() {
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            if(position > 0){
+                customer_State = (String) state.getItemAtPosition(position);
+            }
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+
+        }
+    };
 }
--- app/src/main/java/project/senior/hardhats/CustomerFragment.java
+++ app/src/main/java/project/senior/hardhats/CustomerFragment.java
@@ -1,6 +1,7 @@
 package project.senior.hardhats;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 //import android.app.Fragment;
 import android.support.v4.app.Fragment;
@@ -8,6 +9,7 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -15,6 +17,7 @@ import android.view.ViewGroup;
  */
 public class CustomerFragment extends Fragment {
 
+    Button add_customer;
 
     public CustomerFragment() {
         // Required empty public constructor
@@ -31,6 +34,14 @@ public class CustomerFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        add_customer = (Button) getView().findViewById(R.id.add_customer_button);
+        add_customer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), AddCustomer.class);
+                startActivity(intent);
+            }
+        });
     //change stuff here to give logic to layout
 
     }
