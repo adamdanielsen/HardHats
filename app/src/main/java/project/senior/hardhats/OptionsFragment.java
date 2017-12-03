9937a212f22ed23b9729293c513aedc22ec1e8e4
Evan-E <dudeman43094@gmail.com>
2017-12-03 11:45:40 -0500

    added xml layout elements in options to the fragments code so that we can access the values and set them accordingly

--- app/src/main/java/project/senior/hardhats/OptionsFragment.java
+++ app/src/main/java/project/senior/hardhats/OptionsFragment.java
@@ -7,7 +7,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 //import android.app.Fragment;
 
@@ -18,6 +22,21 @@ import android.widget.Button;
 public class OptionsFragment extends Fragment {
 
     Button logoutButton;
+    EditText usernameEditText;
+    EditText passwordEditText;
+    EditText confirmpasswordEditText;
+    EditText firstNameEditText;
+    EditText lastNameEditText;
+    EditText phoneNumberEditText;
+    EditText faxNumberEditText;
+    EditText licenseNumberEditText;
+    EditText emailAddressEditText;
+    EditText companyNameEditText;
+    EditText streetAddressEditText;
+    EditText cityEditText;
+    EditText zipCodeEditText;
+    Spinner state;
+    String user_state;
     public OptionsFragment() {
         // Required empty public constructor
     }
@@ -33,7 +52,24 @@ public class OptionsFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         //change stuff here to give logic to layout
-
+        usernameEditText =(EditText) getView().findViewById(R.id.option_UserNameEditText);
+        passwordEditText = (EditText) getView().findViewById(R.id.option_PasswordEditText);
+        confirmpasswordEditText =(EditText) getView().findViewById(R.id.option_ConfirmPasswordEditText);
+        firstNameEditText = (EditText) getView().findViewById(R.id.option_firstName_editText);
+        lastNameEditText = (EditText) getView().findViewById(R.id.option_lastName_editText);
+        phoneNumberEditText = (EditText) getView().findViewById(R.id.option_phoneNumber_editText);
+        faxNumberEditText = (EditText) getView().findViewById(R.id.option_faxNumber_editText);
+        licenseNumberEditText = (EditText) getView().findViewById(R.id.option_licenseNumber_editText);
+        emailAddressEditText = (EditText) getView().findViewById(R.id.option_emailAddress_editText);
+        companyNameEditText = (EditText) getView().findViewById(R.id.option_CompanyName_editText);
+        streetAddressEditText = (EditText) getView().findViewById(R.id.option_streetAddress_editText);
+        cityEditText = (EditText) getView().findViewById(R.id.option_city_editText);
+        zipCodeEditText = (EditText) getView().findViewById(R.id.option_zipCode_editText);
+        state = (Spinner) getView().findViewById(R.id.option_state_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.states, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        state.setAdapter(adapter);
+        state.setOnItemSelectedListener(state_listener);
         logoutButton = (Button) getView().findViewById(R.id.fragmentOptions_LogoutButton);
         logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,5 +80,19 @@ public class OptionsFragment extends Fragment {
 
 
     }
+
+    private AdapterView.OnItemSelectedListener state_listener = new AdapterView.OnItemSelectedListener() {
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            if (position > 0) {
+                user_state = (String) state.getItemAtPosition(position);
+            }
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+
+        }
+    };
 
 }
